a2c9cbd07b0533a3e88001c6fef0252865a37abb
Sergio García Jiménez <165771977+SergioGarciaJimenez@users.noreply.github.com>
2024-10-30 14:50:34 +0100

    Cambios hover y js

--- script.js
+++ script.js
@@ -1,10 +1,11 @@
-const features = document.querySelectorAll('.feature');
+const elementsToAnimate = document.querySelectorAll('.feature, .summary img'); // Selecciona features e imagen en summary
+
 window.addEventListener('scroll', () => {
-  features.forEach((feature) => {
-    const featureTop = feature.getBoundingClientRect().top;
+  elementsToAnimate.forEach((element) => {
+    const elementTop = element.getBoundingClientRect().top;
     const windowHeight = window.innerHeight;
-    if (featureTop < windowHeight - 100) {
-      feature.classList.add('show');
+    if (elementTop < windowHeight - 100) {
+      element.classList.add('show');
     }
   });
 });
